07e772cdaa1a003b52f1502241cd567e0b0c058f
Harish <harish_r9@yahoo.co.uk>
2021-02-06 15:51:18 +0000

    - build.gradle - Changed from testCompile to testImplementation - CucumberRunner.java - Updated CucumberOptions with pretty, html paths

--- src/test/java/com/harish/runner/CucumberRunner.java
+++ src/test/java/com/harish/runner/CucumberRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features/",
             glue="com.harish.stepdefs",
-            plugin = {"pretty", "json:target/cucumberReports/json", "html:target/cucumberReports/html"})
+            plugin = {"pretty", "json:target/cucumberReports/json/cucumber.json", "html:target/cucumberReports/html/index.html"})
 public class CucumberRunner {
 }

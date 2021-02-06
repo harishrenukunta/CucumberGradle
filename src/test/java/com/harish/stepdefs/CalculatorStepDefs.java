abe6a7f215b1c9fcc3244da49c1486412a238dd5
Harish <harish_r9@yahoo.co.uk>
2021-02-06 16:20:40 +0000

    - CalculatorStepDefs.java - Updated - Added verification step - Calculations.feature - Added verification step to feature file

--- src/test/java/com/harish/stepdefs/CalculatorStepDefs.java
+++ src/test/java/com/harish/stepdefs/CalculatorStepDefs.java
@@ -2,13 +2,17 @@ package com.harish.stepdefs;
 
 import com.harish.services.CalculatorService;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import lombok.RequiredArgsConstructor;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RequiredArgsConstructor
 public class CalculatorStepDefs extends BaseStep{
 
     private final CalculatorService calculatorService;
+    private int result;
 
     @Given("^user has access to calculator$")
     public void givenUserHasAccess(){
@@ -17,8 +21,14 @@ public class CalculatorStepDefs extends BaseStep{
 
     @When("^add (.*) and (.*)$")
     public void addTwoNumber(final int a, final int b){
-        int result = calculatorService.add(a, b);
-        System.out.println("Result:" + result);
+        result = calculatorService.add(a, b);
+    }
+
+    @Then("verify addition of two numbers in calculator is {int}")
+    public void verifyCalculatorResult(final Integer expectedResult){
+        assertThat(result)
+                .as("Addition of two numbers should be same as expected")
+                .isEqualTo(expectedResult.intValue());
     }
 
 }
--- src/test/resources/features/calculator/Calculations.feature
+++ src/test/resources/features/calculator/Calculations.feature
@@ -3,4 +3,4 @@ Feature: User should be able to perform operations
   Scenario: User should be able to perform all operations
     Given user has access to calculator
     When add 5 and 6
-#    Then the value should be 11
+    Then verify addition of two numbers in calculator is 11
